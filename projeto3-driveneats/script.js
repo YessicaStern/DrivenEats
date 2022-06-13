1b4f7bdcd92284aea346c05b2a9b32c5c39dafe9
YessicaStern <yessica_stern@hotmail.com>
2022-06-12 22:23:33 -0300

    save5

--- projeto3-driveneats/script.js
+++ projeto3-driveneats/script.js
@@ -7,6 +7,7 @@ function addborda(elemento , selecao){
     elemento.classList.add("borda");
 
     addicone(elemento, selecao);
+
    if (selecao==="pratos" ){
     prato=elemento.querySelector("h2").innerHTML;
    } else if (selecao==="bebidas" ){ 
@@ -19,7 +20,7 @@ function addborda(elemento , selecao){
 
 function addicone(elemento, selecao){
 
-    const click=document.querySelector(`.${selecao} .verdinho`);
+    const click=document.querySelector(`.${selecao} .verdinho`); 
     const icone= elemento.querySelector(".icones");
     if(click !==null){
         click.classList.remove("verdinho");
@@ -32,15 +33,34 @@ let bebida=null;
 let sobremesa=null;
 
 function pedido(){
-   /* console.log(sobremesa);
+    /*console.log(sobremesa);
     console.log(bebida);
     console.log(prato);*/
    if ( prato !== null && bebida !== null && sobremesa !== null){ 
-    console.log("oi");
     const finalizar = document.querySelector(".confirme");
     finalizar.innerHTML= "Fechar Pedido";
     finalizar.classList.add("fechar");
     }
+    
 }
 
+function finalizarpedido(elemento){
+    if ( prato !== null && bebida !== null && sobremesa !== null){
+    
+    let body2=document.querySelector(".corpo");
+    body2.classList.add(".fundo");
+    
+   /* let teste=document.querySelector(".top");
+    console.log(teste);
+    
+    let finalizando = document.querySelector("button");
+    console.log(finalizando);
+    elemento.classList.add("fundo");
 
+    teste.classList.add("fundo");*/
+   
+
+   /* finalizando.classList.add("bodyfinal");
+    finalizando.classList.add("painel");*/
+    }
+}
